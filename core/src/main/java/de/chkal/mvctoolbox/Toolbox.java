4d8e25623c8322e50ae2d214b0b5cbcff5274d41
Christian Kaltepoth <christian@kaltepoth.de>
2015-12-29 08:41:09 +0100

    Added CDI dependency and Toolbox instance

---
+++ core/src/main/java/de/chkal/mvctoolbox/Toolbox.java
@@ -0,0 +1,15 @@
+package de.chkal.mvctoolbox;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+/**
+ * This class is the only one annotated with {@link Named}. This way the library
+ * won't pollute the global EL namespace. Users will have to use this class
+ * to access other MVC Toolbox objects (i.e. <code>${toolbox.messages}</code>)
+ */
+@Named
+@ApplicationScoped
+public class Toolbox {
+
+}
