fb01b891a5f904077e110fc187cfcf597d3c4a87
Christian Kaltepoth <christian@kaltepoth.de>
2016-01-24 09:20:14 +0100

    MvcMessages should be an interface

--- core/src/main/java/de/chkal/mvctoolbox/core/message/MvcMessages.java
+++ core/src/main/java/de/chkal/mvctoolbox/core/message/MvcMessages.java
@@ -1,86 +1,28 @@
 package de.chkal.mvctoolbox.core.message;
 
-import javax.mvc.annotation.RedirectScoped;
 import javax.mvc.binding.BindingResult;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
-@RedirectScoped
-public class MvcMessages implements Serializable {
+public interface MvcMessages {
 
-  private static final long serialVersionUID = 1L;
+  MvcMessages add(String text);
 
-  private final List<MvcMessage> messages = new ArrayList<>();
+  MvcMessages add(MvcMessage.Severity severity, String text);
 
-  public MvcMessages add(String text) {
-    return add(new MvcMessage(text));
-  }
+  MvcMessages add(BindingResult bindingResult);
 
-  public MvcMessages add(MvcMessage.Severity severity, String text) {
-    return add(new MvcMessage(severity, text));
-  }
+  MvcMessages add(MvcMessage message);
 
-  public MvcMessages add(BindingResult bindingResult) {
+  List<MvcMessage> getAll();
 
-    bindingResult.getAllBindingErrors().stream()
-        .map(e -> new MvcMessage(MvcMessage.Severity.ERROR, e.getParamName(), e.getMessage()))
-        .forEach(this::add);
+  List<MvcMessage> getInfos();
 
-    /*
-     * FIXME: How to get the parameter the ConstraintViolation is referring to?
-     */
-    bindingResult.getAllViolations().stream()
-        .forEach(v -> {
-          final String path = v.getPropertyPath().toString();
-          final String param = path.substring(path.lastIndexOf('.') + 1);
-          this.add(new MvcMessage(MvcMessage.Severity.ERROR, param, v.getMessage()));
-        });
+  List<MvcMessage> getWarnings();
 
-    return this;
-  }
+  List<MvcMessage> getErrors();
 
-  public MvcMessages add(MvcMessage message) {
-    Objects.requireNonNull(message, "Message must not be null");
-    messages.add(message);
-    return this;
-  }
+  MvcMessage getMessage(String param);
 
-  public List<MvcMessage> getAll() {
-    return Collections.unmodifiableList(messages);
-  }
-
-  public List<MvcMessage> getInfos() {
-    return messages.stream()
-        .filter(m -> m.getSeverity() == MvcMessage.Severity.INFO)
-        .collect(Collectors.toList());
-  }
-
-  public List<MvcMessage> getWarnings() {
-    return messages.stream()
-        .filter(m -> m.getSeverity() == MvcMessage.Severity.WARNING)
-        .collect(Collectors.toList());
-  }
-
-  public List<MvcMessage> getErrors() {
-    return messages.stream()
-        .filter(m -> m.getSeverity() == MvcMessage.Severity.ERROR)
-        .collect(Collectors.toList());
-  }
-
-  public MvcMessage getMessage(String param) {
-    return messages.stream()
-        .filter(m -> Objects.equals(param, m.getParam()))
-        .findFirst().orElse(null);
-  }
-
-  public List<MvcMessage> getMessages(String param) {
-    return messages.stream()
-        .filter(m -> Objects.equals(param, m.getParam()))
-        .collect(Collectors.toList());
-  }
+  List<MvcMessage> getMessages(String param);
 
 }
---
+++ core/src/main/java/de/chkal/mvctoolbox/core/message/MvcMessagesImpl.java
@@ -0,0 +1,96 @@
+package de.chkal.mvctoolbox.core.message;
+
+import javax.mvc.annotation.RedirectScoped;
+import javax.mvc.binding.BindingResult;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@RedirectScoped
+public class MvcMessagesImpl implements Serializable, MvcMessages {
+
+  private static final long serialVersionUID = 1L;
+
+  private final List<MvcMessage> messages = new ArrayList<>();
+
+  @Override
+  public MvcMessages add(String text) {
+    return add(new MvcMessage(text));
+  }
+
+  @Override
+  public MvcMessages add(MvcMessage.Severity severity, String text) {
+    return add(new MvcMessage(severity, text));
+  }
+
+  @Override
+  public MvcMessages add(BindingResult bindingResult) {
+
+    bindingResult.getAllBindingErrors().stream()
+        .map(e -> new MvcMessage(MvcMessage.Severity.ERROR, e.getParamName(), e.getMessage()))
+        .forEach(this::add);
+
+    /*
+     * FIXME: How to get the parameter the ConstraintViolation is referring to?
+     */
+    bindingResult.getAllViolations().stream()
+        .forEach(v -> {
+          final String path = v.getPropertyPath().toString();
+          final String param = path.substring(path.lastIndexOf('.') + 1);
+          this.add(new MvcMessage(MvcMessage.Severity.ERROR, param, v.getMessage()));
+        });
+
+    return this;
+  }
+
+  @Override
+  public MvcMessages add(MvcMessage message) {
+    Objects.requireNonNull(message, "Message must not be null");
+    messages.add(message);
+    return this;
+  }
+
+  @Override
+  public List<MvcMessage> getAll() {
+    return Collections.unmodifiableList(messages);
+  }
+
+  @Override
+  public List<MvcMessage> getInfos() {
+    return messages.stream()
+        .filter(m -> m.getSeverity() == MvcMessage.Severity.INFO)
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public List<MvcMessage> getWarnings() {
+    return messages.stream()
+        .filter(m -> m.getSeverity() == MvcMessage.Severity.WARNING)
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public List<MvcMessage> getErrors() {
+    return messages.stream()
+        .filter(m -> m.getSeverity() == MvcMessage.Severity.ERROR)
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public MvcMessage getMessage(String param) {
+    return messages.stream()
+        .filter(m -> Objects.equals(param, m.getParam()))
+        .findFirst().orElse(null);
+  }
+
+  @Override
+  public List<MvcMessage> getMessages(String param) {
+    return messages.stream()
+        .filter(m -> Objects.equals(param, m.getParam()))
+        .collect(Collectors.toList());
+  }
+
+}
